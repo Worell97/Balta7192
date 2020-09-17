9aa088613c6fabea322f275492d5fffbe2223250
Oscar W. Filho <oscar.filho@eco.centauro>
2020-09-17 11:49:55 -0300

    Finish commit end of course

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
-import 'package:aog/widgets/input.widget.dart';
-import 'package:aog/widgets/logo.widget.dart';
+import 'package:aog/pages/home.page.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_masked_text/flutter_masked_text.dart';
 
 void main() {
   runApp(MyApp());
@@ -19,51 +17,6 @@ class MyApp extends StatelessWidget {
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  var _gasCtrl = new MoneyMaskedTextController();
-  var _alcCtrl = new MoneyMaskedTextController();
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).primaryColor,
-      body: ListView(
-        children: <Widget>[
-          Logo(),
-          Input(
-            ctrl: _gasCtrl,
-            label: 'Gasolina',
-          ),
-          Input(
-            ctrl: _alcCtrl,
-            label: 'Álcool',
-          ),
-          Container(
-            margin: EdgeInsets.all(30),
-            height: 60,
-            decoration: BoxDecoration(
-              color: Colors.white.withOpacity(0.8),
-              borderRadius: BorderRadius.circular(
-                60,
-              ),
-            ),
-            child: FlatButton(
-              onPressed: () {},
-              child: Text(
-                'Calcular',
-                style: TextStyle(
-                  color: Theme.of(context).primaryColor,
-                  fontSize: 25,
-                  fontFamily: 'Big Shoulders Display',
-                ),
-              ),
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
